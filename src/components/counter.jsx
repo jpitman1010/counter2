709ddb5b99dfdd5865cec15cfc8c903118ee856b
jpitman1010 <jpitman1010@gmail.com>
2021-08-30 14:35:03 +0300

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        tags: []
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return "There are no tags!";
+        
+        return  <ul>
+        { this.state.tags.map(tag => <li key={ tag }>{ tag }</li>) }
+    </ul>
+    }
+
+    handleIncrement = () => {
+        console.log('increment clicked', this.state.count)
+    }
+    
+    render() { 
+        return (
+        <React.Fragment>
+            { this.state.tags.length === 0 && "Please create new tag!   " }
+            <span className={this.getBadgeClasses()}>{ this.formatCount() }</span>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        </React.Fragment>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+
+
+    formatCount() {
+        const  { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+}
+ 
+export default Counter;
