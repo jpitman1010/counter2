4dc477e82db89823bcfa20a37b50b9b9e51beec3
jpitman1010 <jpitman1010@gmail.com>
2021-09-02 12:32:51 +0300

    creating stateless functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return (
-<nav className="navbar navbar-light bg-light">
-  <div className="container-fluid">
-    <span className="navbar-brand mb-0 h1">Navbar
-        <span className="badge badge-pill badge secondary">
-            {this.props.totalCounters}
-        </span>
-    </span>
-  </div>
-</nav>
+//use sfc tab for creating a stateless functional component
+//cannot pass props like with a class, so you have to add 
+//props to pass through the function
+const NavBar = (props) => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+          <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Navbar
+                <span className="badge badge-pill badge secondary">
+                    {props.totalCounters}
+                </span>
+            </span>
+          </div>
+        </nav>
+        
+                  );
+};
 
-          );
-    }
-}
- 
 export default NavBar;
